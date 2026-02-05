dfcb9d452ffaf07ce8f5bf616bdef2ce67e9e9e0
Prakhar Vishnoi <pravishn2@publicisgroupe.net>
2026-02-05 13:09:12 +0530

    adding new code

---
+++ section7/accounts/src/main/java/com/microservice/accounts/service/ICustomersService.java
@@ -0,0 +1,13 @@
+package com.microservice.accounts.service;
+
+import com.microservice.accounts.dto.CustomerDetailsDto;
+
+public interface ICustomersService {
+
+    /**
+     *
+     * @param mobileNumber - Input Mobile Number
+     * @return Customer Details based on a given mobileNumber
+     */
+    CustomerDetailsDto fetchCustomerDetails(String mobileNumber);
+}
